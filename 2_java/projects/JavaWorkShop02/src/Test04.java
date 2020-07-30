aee968f4b13fff301495036281b0f3ff7a54abd3
Woojo <kwoojo0127@naver.com>
2020-07-30 16:15:20 +0900

    Add workshop2 quiz 1, 2 solutions

---
+++ 2_java/projects/JavaWorkShop02/src/Test04.java
@@ -0,0 +1,24 @@
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= 6; j++) {
+				if (i + j == 6) {
+					System.out.println(i + " + " + j + " = " + (i + j));
+				}
+			}
+		}
+
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= 6; j++) {
+				for (int k = 1; k <= 6; k++) {
+					if (i + j + k == 6) {
+						System.out.println(i + " + " + j + " + " + k + " = " + (i + j + k));
+					}
+				}
+			}
+		}
+	}
+
+}
