7d296a3beb6aa2a57f02f5e2b494ce688c506896
u-zo <kwoojo0127@naver.com>
2020-09-13 19:07:13 +0900

    Add servlet codes for servlet, jsp study

---
+++ 3_jsp/src/HelloTest/src/com/controller/HelloTest.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloTest")
+public class HelloTest extends HttpServlet {
+
+  // GET 방식
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.getWriter().append("Served at: ").append(request.getContextPath());
+    System.out.println("Hello World!");
+  }
+
+  // POST 방식
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request, response);
+  }
+
+}
